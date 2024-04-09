0ac163ed57b3d757d0eacdeb60d567e0e0058278
Zoheb Hasan <zohebhasanofficial@gmail.com>
2024-04-09 00:55:00 -0400

    login Page ongoing

--- client/src/components/loginSignup/login/bodyLogin.jsx
+++ client/src/components/loginSignup/login/bodyLogin.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom'
+
 import videoSrc from './assets/poster.mp4';
+
+
 import '../../../stylesheets/App.css'; 
 import '../../../stylesheets/loginSignup/login/loginBody.css';
+import '../../../stylesheets/elements/conna.css';
 
-
+import ShowSign from './assets/show.png';
+import HideSign from './assets/hide.png';
+import GoogleLogo from './assets/google.png';
+import MicrosoftLogo from './assets/microsoft.png'
+import AppleLightLogo from './assets/appleLogoBlack.png';
+import AppleDarkLogo from './assets/appleLogoWhite.png';
 import LogoDark from '../../assets/logoDark.png';
 import LogoLight from '../../assets/logoLight.png'; 
+
+import BackgroundLogin from './background/backgroundLogin';
 
 
 function PosterContainer({isDarkMode}){
@@ -21,8 +33,9 @@ function PosterContainer({isDarkMode}){
                     </div>
                     <div className="posterText">
                         <p > 
-                            The <span className='span1'>Future</span><br />
-                            is <span className='span2'>Here</span>.
+                            The <span className='freedomSpan'>Freedom</span><br />
+                            is <span className='hereSpan'>Here</span>.<br/>
+                            <span className = 'uncontrolledSpan'>Uncontrolled</span>. 
                         </p>
                     </div>
                    
@@ -32,24 +45,84 @@ function PosterContainer({isDarkMode}){
         </>
     );
 }
-
-function LoginContainer(){
-    return(
+function LoginContainer() {
+    const [showPassword, setShowPassword] = useState(false);
+  
+    const togglePasswordVisibility = () => {
+      setShowPassword(!showPassword);
+    };
+  
+    return (
         <>
             <div className="loginContainer">
-                This is login Container
-                <div className="login">
-                    This is login element
+                {/* <BackgroundLogin/> */}
+                <div className = "loginText">
+                    <p> 
+                        Let's get 
+                        you <span className='in'>in</span>.
+                    </p>
                 </div>
+                <div className="login">          
+                    <div className="credentialContainer">    
+                        <div className="idContainer">
+                            <input type="text" id="emailInput" placeholder=" " />
+                            <label for="emailInput">Phone, email, or username</label>
+                        </div>
+                        <div className="passwordContainer">
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                id="passwordInput"
+                                placeholder=" "
+                            />
+                            <label for="passwordInput">Password</label>
+                            <button onClick={togglePasswordVisibility} className="togglePasswordBtn">
+                                <img src={showPassword ? ShowSign : HideSign} alt={showPassword ? "Hide" : "Show"} />
+                            </button>
+
+                        </div>        
+                    </div>
+
+
+                    <div className = "loginBtnContainer">
+                        <Link to = "#" className="loginBtn">Sign in</Link>
+                    </div>    
+
+
+                    <div className="socialLoginButtons">
+                        <p> Sign in With: </p>
+                        <Link to = "#" className="microsoftBtn">
+                            <img src={MicrosoftLogo} alt="Microsoft" />
+                        </Link>
+                        <Link className="appleBtn">
+                            <img src={AppleLightLogo} alt="Apple" />
+                        </Link>
+                        <Link className="googleBtn">
+                            <img src={GoogleLogo} alt="Google" />
+                        </Link>
+                    </div>
+                         
+                    <div className="dividerContainer">
+                        <div className="line"></div>
+                            <span className="orText">or</span>
+                        <div className="line"></div>
+                    </div>
+                    <div className = "signupBtnContainer">
+                        <Link className="signupBtn">Create an account</Link>  
+                    </div>
+                              
+                </div>
+                    
                 <div className="bottomContainer">
-                    <div className="connaContainer">
+                    <div className="connaLogin">
                         <button className="connaBtn">Conna</button>
                     </div>
                 </div>
             </div>
         </>
+     
     );
-}
+  }
+  
 
 
 export default function Body({isDarkMode}) {
