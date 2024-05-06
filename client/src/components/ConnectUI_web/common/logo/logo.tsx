82944267ee795e9c70f4b146eacbeeb62bbf50c2
Zoheb Hasan <zohebhasanofficial@gmail.com>
2024-05-06 04:37:02 -0400

    transferring to PC

--- client/src/components/ConnectUI_web/common/logo/logo.tsx
+++ client/src/components/ConnectUI_web/common/logo/logo.tsx
@@ -3,21 +3,22 @@ import styled from "styled-components";
 
 import LogoDark from '../../../assets/logoDark.png';
 import LogoLight from '../../../assets/logoLight.png'; 
+import LogoNew from '../../../assets/logo.png'
 
 import { useDarkMode } from "../../../../contexts/DarkMode/DarkMode";
 
 const Logo: React.FC = () => {
     const {isDarkMode} = useDarkMode();
     return(
-        <StyledLogo src={isDarkMode ? LogoDark : LogoLight} alt="Logo"/>
+        // <StyledLogo src={isDarkMode ? LogoDark : LogoLight} alt="Logo"/>
+        <StyledLogo src={LogoNew} alt="Logo"/>
     );
 }
 
 export default Logo;
 
 const StyledLogo = styled.img`
-    width: 2.375rem; 
-    height: auto;
+    width: 3.5rem;
     opacity: 1;
     transition: transform 0.5s, opacity 0.5s;
     mix-blend-mode: multiply;
