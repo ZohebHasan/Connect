bcb3d8ebf6a78bd2766288cbe363ffbb23b45e03
Zoheb Hasan <zohebhasanofficial@gmail.com>
2024-05-20 19:55:59 -0400

    Signup Process is operational, Signal Protocol key generation works

--- client/src/security/profile_content_encryption.ts
+++ client/src/security/profile_content_encryption.ts
@@ -1,31 +1,31 @@
-// import { randomBytes, createCipheriv, createDecipheriv, publicEncrypt, privateDecrypt } from 'crypto';
+import { randomBytes, createCipheriv, createDecipheriv, publicEncrypt, privateDecrypt } from 'crypto';
 
-// const algorithm = 'aes-256-ctr';
+const algorithm = 'aes-256-ctr';
 
-// function generateSymmetricKey(): Buffer {
-//     return randomBytes(32);
-// }
+function generateSymmetricKey(): Buffer {
+    return randomBytes(32);
+}
 
-// function encryptContent(content: string, key: Buffer): string {
-//     const iv = randomBytes(16);
-//     const cipher = createCipheriv(algorithm, key, iv);
-//     const encrypted = Buffer.concat([cipher.update(content, 'utf8'), cipher.final()]);
-//     return `${iv.toString('hex')}:${encrypted.toString('hex')}`;
-// }
+function encryptContent(content: string, key: Buffer): string {
+    const iv = randomBytes(16);
+    const cipher = createCipheriv(algorithm, key, iv);
+    const encrypted = Buffer.concat([cipher.update(content, 'utf8'), cipher.final()]);
+    return `${iv.toString('hex')}:${encrypted.toString('hex')}`;
+}
 
-// function decryptContent(encryptedContent: string, key: Buffer): string {
-//     const [iv, encrypted] = encryptedContent.split(':').map((part) => Buffer.from(part, 'hex'));
-//     const decipher = createDecipheriv(algorithm, key, iv);
-//     const decrypted = Buffer.concat([decipher.update(encrypted), decipher.final()]);
-//     return decrypted.toString('utf8');
-// }
+function decryptContent(encryptedContent: string, key: Buffer): string {
+    const [iv, encrypted] = encryptedContent.split(':').map((part) => Buffer.from(part, 'hex'));
+    const decipher = createDecipheriv(algorithm, key, iv);
+    const decrypted = Buffer.concat([decipher.update(encrypted), decipher.final()]);
+    return decrypted.toString('utf8');
+}
 
-// function encryptSymmetricKey(symmetricKey: Buffer, publicKey: Buffer): Buffer {
-//     return publicEncrypt(publicKey, symmetricKey);
-// }
+function encryptSymmetricKey(symmetricKey: Buffer, publicKey: Buffer): Buffer {
+    return publicEncrypt(publicKey, symmetricKey);
+}
 
-// function decryptSymmetricKey(encryptedKey: Buffer, privateKey: Buffer): Buffer {
-//     return privateDecrypt(privateKey, encryptedKey);
-// }
+function decryptSymmetricKey(encryptedKey: Buffer, privateKey: Buffer): Buffer {
+    return privateDecrypt(privateKey, encryptedKey);
+}
 
-// export { generateSymmetricKey, encryptContent, decryptContent, encryptSymmetricKey, decryptSymmetricKey };
+export { generateSymmetricKey, encryptContent, decryptContent, encryptSymmetricKey, decryptSymmetricKey };
--- server/dist/models/userModel.js
+++ server/dist/models/userModel.js
@@ -1,6 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("mongoose");
+const preKeySchema = new mongoose_1.Schema({
+    keyId: { type: Number, required: true },
+    publicKey: { type: String, required: true }
+});
+const signedPreKeySchema = new mongoose_1.Schema({
+    keyId: { type: Number, required: true },
+    publicKey: { type: String, required: true },
+    signature: { type: String, required: true }
+});
 const schema = new mongoose_1.Schema({
     fullName: { type: String, required: true },
     email: { type: String, unique: true, sparse: true },
@@ -11,10 +20,16 @@ const schema = new mongoose_1.Schema({
     phoneNumber: { type: String, unique: true, sparse: true },
     dataProtection: { type: Boolean, default: true },
     profileEncryption: { type: Boolean, default: true },
-    contentMonitization: { type: Boolean, default: true },
+    contentMonetization: { type: Boolean, default: true },
     censor: { type: Boolean, default: false },
     restricted: { type: Boolean, default: false },
     age: { type: Number, required: false },
-    dob: { type: Date, required: false }
+    dob: { type: Date, required: false },
+    keys: {
+        identityPublicKey: { type: String, required: true },
+        registrationId: { type: Number, required: true },
+        preKeys: [preKeySchema],
+        signedPreKey: signedPreKeySchema
+    }
 });
 exports.default = (0, mongoose_1.model)('User', schema);
