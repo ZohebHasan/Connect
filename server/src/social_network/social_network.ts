e42c8add048a5da7f5e4898a32a6bfa08bdfbc8f
kam-stand <kamrul.hassan@stonybrook.edu>
2024-07-28 22:34:51 -0400

    created socail network schema and design outline

---
+++ server/src/social_network/social_network.ts
@@ -0,0 +1,20 @@
+/**
+ * 1. if encryyoted profile run a recommendation algorithm based off of document similarity via mongoDB
+ * 2. if not encrypted:
+ *      a. run the googole vision api to extract labels(tags) from the image
+ *      b. run a recommendation algorithm based off labels extracted from the image
+ * 3. send the repsonse back to the client as a list of post objects
+ *
+ * 
+ */
+
+
+
+/**
+ * search how to find similar documents in mongoDB
+ * 1. obtain meta data from the user's profile
+ *    a. obtain the amount of likes, views , comments, shares and the date of the post
+ *     
+ * 
+ * 
+ */
